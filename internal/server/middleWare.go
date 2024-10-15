4704c87cf2e36eee3142376e60119f8a4f2bdbba
Hendrik Voßkamp <hendrik.vosskamp@gmail.com>
2024-10-15 22:40:26 +0200

    wip

--- internal/server/middleWare.go
+++ internal/server/middleWare.go
@@ -1,19 +1,18 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func CheckSession(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("session")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusForbidden)
-			return
-		}
-		sessionToken := cookie.Value
-		fmt.Println(sessionToken)
+		// cookie, err := r.Cookie("session")
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusForbidden)
+		// 	return
+		// }
+		// sessionToken := cookie.Value
+		// fmt.Println(sessionToken)
 		next.ServeHTTP(w, r)
 	})
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"vosskamp-reisen-3/internal/models"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -13,6 +14,9 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux.HandleFunc("/", s.HomeHandler)
 	mux.HandleFunc("/login", s.LoginHandler)
 	mux.Handle("/cms/users", CheckSession(http.HandlerFunc(s.fetchUsersHandler)))
+	mux.Handle("GET /cms/users/form", http.HandlerFunc(s.fetchUserFormHandler))
+	mux.Handle("POST /cms/users/form", http.HandlerFunc(s.createUserFormHandler))
+	// mux.Handle("/cms/users/form", CheckSession(http.HandlerFunc(s.fetchUserFormHandler)))
 
 	mux.HandleFunc("/health", s.healthHandler)
 
@@ -53,6 +57,69 @@ func (s *Server) fetchUsersHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(users)
+}
+
+func (s *Server) fetchUserFormHandler(w http.ResponseWriter, r *http.Request) {
+	err := s.tmpl.ExecuteTemplate(w, "userCreation", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Server) createUserFormHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user := models.Users{
+		FirstName: r.FormValue("first-name"),
+		LastName:  r.FormValue("last-name"),
+		Email:     r.FormValue("email"),
+		Password:  r.FormValue("password"),
+	}
+	fmt.Print(user)
+
+	var errorMessages = map[string]bool{
+		"firstName": false,
+		"lastName":  false,
+		"email":     false,
+		"password":  false,
+	}
+	hasErrors := false
+	if user.FirstName == "" {
+		errorMessages["firstName"] = true
+		hasErrors = true
+	}
+	if user.LastName == "" {
+		errorMessages["lastName"] = true
+		hasErrors = true
+	}
+	if user.Email == "" {
+		errorMessages["email"] = true
+		hasErrors = true
+	}
+	if user.Password == "" {
+		errorMessages["password"] = true
+		hasErrors = true
+	}
+	fmt.Print(errorMessages)
+	if hasErrors {
+		// s.tmpl.ExecuteTemplate(w, "authErrors", errorMessages)
+		s.tmpl.ExecuteTemplate(w, "userForm", errorMessages)
+		return
+	}
+
+	_, err = s.userService.CreateUser(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("HX-Location", "/")
+	w.WriteHeader((http.StatusNoContent))
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
